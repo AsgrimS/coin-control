8850f3187518d8c567b7309d4167a42d29e3661a
AsgrimS <jan.semik@gmail.com>
2024-06-24 12:32:10 +0200

    migrate to eslint flat config

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,12 +3,14 @@ import "unplugin-icons/types/svelte"
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	interface Locals {
-		user: import("lucia").User | null
-		session: import("lucia").Session | null
+declare global {
+	declare namespace App {
+		interface Locals {
+			user: import("lucia").User | null
+			session: import("lucia").Session | null
+		}
+		// interface PageData {}
+		// interface Error {}
+		// interface Platform {}
 	}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
 }
