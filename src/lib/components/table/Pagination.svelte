df08e285c2191572effcb783e18863ce4febe2ff
AsgrimS <jan.semik@gmail.com>
2024-07-15 16:30:53 +0200

    remove desktop pagination

--- src/lib/components/table/Pagination.svelte
+++ src/lib/components/table/Pagination.svelte
@@ -6,58 +6,25 @@
 	export let handler: DataHandler
 	const pageNumber = handler.getPageNumber()
 	const pageCount = handler.getPageCount()
-	const pages = handler.getPages({ ellipsis: true })
 
 	pageCount.subscribe((value) => {
 		if ($pageNumber > value) handler.setPage(value)
 	})
 </script>
 
-<!-- Desktop buttons -->
-<section class="variant-ghost-surface btn-group hidden lg:flex [&>*+*]:border-surface-500">
+<section>
 	<button
 		type="button"
-		class="hover:variant-soft-primary"
-		class:disabled={$pageNumber === 1}
-		on:click={() => handler.setPage("previous")}
-	>
-		<CaretLeftIcon />
-	</button>
-	{#each $pages as page}
-		<button
-			type="button"
-			class="hover:variant-soft-primary"
-			class:active={$pageNumber === page}
-			class:ellipse={page === null}
-			on:click={() => handler.setPage(page)}
-		>
-			{page ?? "..."}
-		</button>
-	{/each}
-	<button
-		type="button"
-		class="hover:variant-soft-primary"
-		class:disabled={$pageNumber === $pageCount}
-		on:click={() => handler.setPage("next")}
-	>
-		<CaretRightIcon />
-	</button>
-</section>
-
-<!-- Mobile buttons -->
-<section class="lg:hidden">
-	<button
-		type="button"
-		class="variant-ghost-surface btn btn-icon mr-2 hover:variant-soft-primary"
-		class:disabled={$pageNumber === 1}
+		class="variant-ghost-surface btn btn-icon mr-2"
+		disabled={$pageNumber === 1}
 		on:click={() => handler.setPage("previous")}
 	>
 		<CaretLeftIcon />
 	</button>
 	<button
 		type="button"
-		class="variant-ghost-surface btn-icon hover:variant-soft-primary"
-		class:disabled={$pageNumber === $pageCount}
+		class="variant-ghost-surface btn-icon"
+		disabled={$pageNumber === $pageCount}
 		on:click={() => handler.setPage("next")}
 	>
 		<CaretRightIcon />
