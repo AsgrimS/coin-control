0ef626fa877193e550c0379f5d4f25e6f40ee48f
AsgrimS <jan.semik@gmail.com>
2024-11-16 15:53:09 +0100

    init

---
+++ shell.nix
@@ -0,0 +1,33 @@
+let
+  pkgs = import <nixpkgs> { };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs;[
+    at-spi2-atk
+    atkmm
+    cairo
+    gdk-pixbuf
+    glib
+    gobject-introspection
+    gobject-introspection.dev
+    gtk3
+    harfbuzz
+    librsvg
+    libsoup_3
+    pango
+    webkitgtk_4_1
+    webkitgtk_4_1.dev
+    pkg-config
+    rustup
+    rust-analyzer
+    nodejs_22
+    pnpm
+  ];
+
+  OPENSSL_NO_VENDOR = 1;
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [ openssl ]);
+  OPENSSL_LIB_DIR = "${pkgs.lib.getLib pkgs.openssl}/lib";
+  OPENSSL_DIR = "${pkgs.openssl.dev}";
+  PKG_CONFIG_PATH = with pkgs; "${glib.dev}/lib/pkgconfig:${libsoup_3.dev}/lib/pkgconfig:${webkitgtk_4_1.dev}/lib/pkgconfig:${at-spi2-atk.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${gdk-pixbuf.dev}/lib/pkgconfig:${cairo.dev}/lib/pkgconfig:${pango.dev}/lib/pkgconfig:${harfbuzz.dev}/lib/pkgconfig";
+  WEBKIT_DISABLE_DMABUF_RENDERER = 1;
+}
---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,27 @@
+use specta_typescript::Typescript;
+use tauri_specta::{collect_commands, Builder};
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+#[specta::specta]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    let builder = Builder::<tauri::Wry>::new()
+        // Then register them (separated by a comma)
+        .commands(collect_commands![greet,]);
+
+    #[cfg(debug_assertions)] // <- Only export on non-release builds
+    builder
+        .export(Typescript::default(), "../src/lib/bindings.ts")
+        .expect("Failed to export typescript bindings");
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(builder.invoke_handler())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
