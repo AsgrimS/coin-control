31ec4dcd2246066582135b0befc4006049eab65b
AsgrimS <jan.semik@gmail.com>
2025-02-02 09:33:12 +0100

    setup rust targets

--- devenv.nix
+++ devenv.nix
@@ -27,6 +27,13 @@
   languages = {
     rust = {
       enable = true;
+      channel = "stable";
+      targets = [
+      "aarch64-linux-android"
+      "armv7-linux-androideabi"
+      "i686-linux-android"
+      "x86_64-linux-android"
+      ];
     };
     javascript = {
       enable = true;
