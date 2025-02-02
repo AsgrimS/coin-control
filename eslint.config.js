63cb0b68122fb853fadcd6da6a863942541fcd95
AsgrimS <jan.semik@gmail.com>
2025-02-02 08:27:34 +0100

    formatter

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
-import prettier from "eslint-config-prettier";
-import js from '@eslint/js';
-import { includeIgnoreFile } from '@eslint/compat';
-import svelte from 'eslint-plugin-svelte';
-import globals from 'globals';
-import { fileURLToPath } from 'node:url';
-import ts from 'typescript-eslint';
-const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
+import prettier from "eslint-config-prettier"
+import js from "@eslint/js"
+import { includeIgnoreFile } from "@eslint/compat"
+import svelte from "eslint-plugin-svelte"
+import globals from "globals"
+import { fileURLToPath } from "node:url"
+import ts from "typescript-eslint"
+const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url))
 
 export default ts.config(
   includeIgnoreFile(gitignorePath),
@@ -13,25 +13,25 @@ export default ts.config(
   ...ts.configs.recommended,
   ...svelte.configs["flat/recommended"],
   prettier,
-  ...svelte.configs['flat/prettier'],
+  ...svelte.configs["flat/prettier"],
   {
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node
-      }
-    }
+        ...globals.node,
+      },
+    },
   },
   {
     files: ["**/*.svelte"],
 
     languageOptions: {
       parserOptions: {
-        parser: ts.parser
-      }
-    }
+        parser: ts.parser,
+      },
+    },
   },
   {
     ignores: ["build/", ".svelte-kit/", "dist/", "src-tauri/", "static", "android-sdk/"],
   }
-);
+)
