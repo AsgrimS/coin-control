1e19ed2c986f834a5fcbc3d6f99a97a1eaca4418
AsgrimS <jan.semik@gmail.com>
2025-02-23 18:33:47 +0100

    BudgetCircle base

---
+++ src/lib/components/BudgetCircle/BudgetCircle.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { ProgressRing, Progress } from "@skeletonlabs/skeleton-svelte"
+  import Coins from "lucide-svelte/icons/coins"
+
+  interface Props {
+    currentSpendings: number
+    budget: number
+  }
+
+  let { budget, currentSpendings }: Props = $props()
+</script>
+
+<div class="grid grid-cols-1 justify-center gap-4">
+  <ProgressRing value={currentSpendings} max={budget}>
+    <Coins size={48} />
+  </ProgressRing>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-  import HandCoins from "lucide-svelte/icons/hand-coins"
+  import BudgetCircle from "$lib/components/BudgetCircle/BudgetCircle.svelte"
 </script>
 
 <!-- Content -->
 <div class="flex h-full items-center justify-center">
-  <p class="opacity-20"><HandCoins size="128" /></p>
-  <p class="ml-4 opacity-20">Coin Control</p>
+  <BudgetCircle budget={10} currentSpendings={3} />
 </div>
